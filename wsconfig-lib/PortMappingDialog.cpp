cb1f993d806bf848120f1761cf7b4ad8dcee42b3
sallecta <sallecta@inbox.ru>
2021-03-15 22:44:37 +0300

    lot of _ASSERT was not declared in this scope #23

--- wsconfig-lib/PortMappingDialog.cpp
+++ wsconfig-lib/PortMappingDialog.cpp
@@ -21,7 +21,7 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //-------------------------------------------------------------------------
 //
-
+#include <crtdbg.h>
 #include "PortMappingDialog.h"
 #include "EditPortMappingDialog.h"
 #include "ConfigDialog.h"
--- wsconfig-lib/UIDataAccess.cpp
+++ wsconfig-lib/UIDataAccess.cpp
@@ -21,7 +21,7 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //-------------------------------------------------------------------------
 //
-
+#include <crtdbg.h>
 #include "UIDataAccess.h"
 
 #include "util/StringParser.h"
