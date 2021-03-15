12318dc24c0a6a5e7609cae386b84d7e47a7c760
sallecta <sallecta@inbox.ru>
2021-03-15 22:37:33 +0300

    _ASSERT was not declared in this scope #22

--- wsconfig-lib/IpAccessControlDialog.cpp
+++ wsconfig-lib/IpAccessControlDialog.cpp
@@ -21,7 +21,7 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //-------------------------------------------------------------------------
 //
-
+#include <crtdbg.h>
 #include "tvnserver/resource.h"
 #include "IpAccessControlDialog.h"
 #include "ConfigDialog.h"
