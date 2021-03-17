94e6301e92f112bb9d5f577eb380a6a6fd0cd414
sallecta <sallecta@inbox.ru>
2021-03-18 01:59:31 +0300

    _ASSERT was not declared in this scope #35

--- gui/drawing/BitmapGraphics.cpp
+++ gui/drawing/BitmapGraphics.cpp
@@ -21,7 +21,7 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //-------------------------------------------------------------------------
 //
-
+#include <crtdbg.h>
 #include "BitmapGraphics.h"
 
 BitmapGraphics::BitmapGraphics(DeviceContext *complatibleDC, int width, int height)
